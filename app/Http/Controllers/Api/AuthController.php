ea3a092a3f3eb9cab11b427cc9b6277f7366dd1e
marcosborg <m.borges.mail@gmail.com>
2024-06-05 14:16:50 +0100

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
 
         curl_close($curl);
 
-        return $response;
+        return $response['ParsedResults'];
     }
 
     public function getUser(Request $request)
