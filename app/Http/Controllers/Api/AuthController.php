ac5cdf092253da0b1bba0360c9730ef176abf406
marcosborg <m.borges.mail@gmail.com>
2024-06-05 14:20:40 +0100

    Update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
 
         curl_close($curl);
 
-        return $response['ParsedResults']['ParsedResults'];
+        return $response['ParsedResults'][0];
         
     }
 
