cdc31c1d7c8dfd46058150e4a9bd8ec01e0800c2
marcosborg <m.borges.mail@gmail.com>
2024-06-12 16:47:06 +0100

    Update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -244,8 +244,7 @@ class AuthController extends Controller
     {
         // Verifica se a imagem e o product_id estão presentes
         if ($request->hasFile('image') && $request->has('product_id')) {
-            //return $this->savePhoto($request);
-            return 'ok';
+            return $this->savePhoto($request);
         } else {
             return response()->json(['error' => 'Faltam parâmetros obrigatórios.'], 400);
         }
