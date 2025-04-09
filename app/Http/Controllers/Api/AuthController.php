bf44b131a0f0f72d9f875810e568205d5f912db5
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2025-04-09 14:23:36 +0100

    Add OCR API integration in sendPhoto method and remove commented code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -65,6 +65,8 @@ class AuthController extends Controller
 
         $curl = curl_init();
 
+        /*
+
         curl_setopt_array(
             $curl,
             array(
@@ -77,6 +79,24 @@ class AuthController extends Controller
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'GET',
             )
+        );
+
+        */
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.ocr.space/parse/image',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('url' => $image, 'OCREngine' => '2'),
+            CURLOPT_HTTPHEADER => array(
+                'apikey: ' . env('OCR_API')
+            ),
+        )
         );
 
         $response = curl_exec($curl);
